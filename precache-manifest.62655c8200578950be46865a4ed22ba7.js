86d76042c67b4c2ba8e6633f2d1b55411d84f58c
Atsuko <ashimiz1@binghamton.edu>
2019-01-13 19:43:33 -0500

    Updates

---
+++ precache-manifest.62655c8200578950be46865a4ed22ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bdfd9b3ab5b837e207",
+    "url": "/studyabroadscheduler/static/js/runtime~main.a3bdfd9b.js"
+  },
+  {
+    "revision": "64d20a2ddfabdc423737",
+    "url": "/studyabroadscheduler/static/js/main.64d20a2d.chunk.js"
+  },
+  {
+    "revision": "0bb36b9e6d99e218023b",
+    "url": "/studyabroadscheduler/static/js/1.0bb36b9e.chunk.js"
+  },
+  {
+    "revision": "64d20a2ddfabdc423737",
+    "url": "/studyabroadscheduler/static/css/main.858ee8dc.chunk.css"
+  },
+  {
+    "revision": "0bb36b9e6d99e218023b",
+    "url": "/studyabroadscheduler/static/css/1.279b19cd.chunk.css"
+  },
+  {
+    "revision": "6fb05a58c85ba14b66a87eace47c2d00",
+    "url": "/studyabroadscheduler/index.html"
+  }
+];
